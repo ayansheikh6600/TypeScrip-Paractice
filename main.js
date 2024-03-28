9d31e9e9ad3aa9a940e30ef79be28af601ae2f0a
ayansheikh6600 <ayansheikh6600@gmail.com>
2024-03-29 03:15:52 +0500

    function Done

---
+++ main.js
@@ -0,0 +1,11 @@
+var myFun = function (Name, Age) {
+    console.log(Name + " " + Age);
+    console.log("".concat(Name, " ").concat(Age));
+    console.log(Age);
+};
+myFun("aya", 18);
+myFun("aya", 18);
+console.log("ayyan");
+console.log("ayyan");
+console.log("ayyan");
+// javaScript file ko watch mode pr lagany ka Package Name : nodemon
---
+++ main.ts
@@ -0,0 +1,17 @@
+let myFun = (Name: String, Age:number) => {
+  console.log(Name + " " + Age);
+  console.log(`${Name} ${Age}`);
+  console.log(Age);
+};
+myFun("aya", 18);
+myFun("aya", 18);
+
+console.log("ayyan");
+console.log("ayyan");
+console.log("ayyan");
+
+
+
+// javaScript file ko watch mode pr lagany ka Package Name : nodemon
+
+
